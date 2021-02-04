862572428f0bbb402e9dc0b1e7f8a68998d8dd4a
5h1ro <rajamodus7073@gmail.com>
2021-02-04 20:24:20 +0700

    Buy Activity

---
+++ app/src/main/java/com/tjeret99/kasirku/BuyActivity.kt
@@ -0,0 +1,11 @@
+package com.tjeret99.kasirku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BuyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy)
+    }
+}
