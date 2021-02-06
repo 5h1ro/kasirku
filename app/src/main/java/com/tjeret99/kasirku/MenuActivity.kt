996702b51298f2cc7ebf7abd1fcc32d6ed20a925
5h1ro <rajamodus7073@gmail.com>
2021-02-06 22:16:01 +0700

    Main fix

--- app/src/main/java/com/tjeret99/kasirku/MenuActivity.kt
+++ app/src/main/java/com/tjeret99/kasirku/MenuActivity.kt
@@ -6,29 +6,31 @@ import android.os.Bundle
 import android.util.Log
 import android.util.TypedValue
 import android.view.Gravity
-import android.widget.*
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.android.volley.Request
 import com.android.volley.RequestQueue
-import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONException
+import org.json.JSONObject
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MenuActivity : AppCompatActivity() {
 
 
     lateinit var layout: LinearLayout
-    public lateinit var namaProduk: String
-    public lateinit var priceProduk: String
     public lateinit var priceAPI: String
     public lateinit var nameAPI: String
     public lateinit var hasil: String
-    public lateinit var btnBuy : LinearLayout
-    public var hasilName = ArrayList<HashMap<String, String>>()
-    public var hasilPrice = ArrayList<HashMap<String, String>>()
+    var listName = ArrayList<String>()
+    var listPrice = ArrayList<String>()
     public lateinit var mQueue: RequestQueue
 //    val url = "http://192.168.9.111:8000/api/product"
     val url = "https://pajuts.000webhostapp.com/produk.php"
@@ -38,7 +40,6 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         layout = findViewById(R.id.parent)
-        btnBuy = findViewById(R.id.buy)
 
         mQueue = Volley.newRequestQueue(this)
         jsonParse()
@@ -47,33 +48,25 @@ class MenuActivity : AppCompatActivity() {
 
 
     fun jsonParse() {
-        val request = JsonObjectRequest(
-                Request.Method.GET, url, null,
-                { response ->
-                    try {
-                        val jsonArray = response.getJSONArray("data")
-                        for (i in 0 until jsonArray.length()) {
-                            val result = jsonArray.getJSONObject(i)
+        val request = StringRequest(Request.Method.GET, url, { response ->
+            try {
+                val jsonObject = JSONObject(response)
+                val a = jsonObject.getJSONArray("data")
+                for (i in 0 until a.length()) {
+                    val c = a.getJSONObject(i)
 
-                            nameAPI = result.getString("name")
-                            priceAPI = result.getString("price")
-                            val dataName = HashMap<String, String>()
-                            val dataPrice = HashMap<String, String>()
+                    nameAPI = c.getString("name")
+                    priceAPI = c.getString("price")
 
-                            dataName["name"] = nameAPI
-                            dataPrice["price"] = priceAPI
+                    listName.add(nameAPI)
+                    listPrice.add(priceAPI)
 
-                            hasilName.add(dataName)
-                            hasilPrice.add(dataPrice)
-                            namaProduk = nameAPI
-                            priceProduk = priceAPI
-                            produk("test")
-//                        break
-                        }
-                    } catch (e: JSONException) {
-                        e.printStackTrace()
-                    }
-                }) { error ->
+                }
+                produk("test")
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }) { error ->
             Log.i("LOG", "Volley Error : " + error.message)
             error.printStackTrace() }
         mQueue.add(request)
@@ -93,89 +86,81 @@ class MenuActivity : AppCompatActivity() {
 
     fun produk(textName: String) {
 
-        btnBuy.setOnClickListener {
 
-            val intent = Intent(this, BuyActivity::class.java)
-            intent.putExtra("name", namaProduk)
-            intent.putExtra("price", priceProduk)
-            startActivity(intent)
+        for (i in 0 until listName.size){
+            val parent = LinearLayout(this)
+            val linear_parent = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+            parent.gravity = Gravity.CENTER
+            linear_parent.gravity = Gravity.CENTER
+            parent.setLayoutParams(linear_parent)
+            parent.orientation = LinearLayout.HORIZONTAL
+            parent.setLayoutParams(linear_parent)
+            parent.setOnClickListener {
+                val intent = Intent(this, BuyActivity::class.java)
+                intent.putExtra("name", listName[i])
+                intent.putExtra("price", listPrice[i])
+                startActivity(intent)
+            }
+
+            val product_pack = LinearLayout(this)
+            val linear_product_pack = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+            product_pack.gravity = Gravity.CENTER
+            linear_product_pack.gravity = Gravity.CENTER
+            product_pack.orientation = LinearLayout.HORIZONTAL
+            product_pack.setLayoutParams(linear_product_pack)
+
+            parent.addView(product_pack)
+
+
+            val product1 = LinearLayout(this)
+            val linear_product1 = LinearLayout.LayoutParams(500, 500)
+            linear_product1.setMargins(50, 50, 50, 50)
+            product1.gravity = Gravity.CENTER
+            product1.setBackgroundColor(getColor(R.color.colorSecondary))
+            product1.orientation = LinearLayout.VERTICAL
+            product1.setLayoutParams(linear_product1)
+
+            product_pack.addView(product1)
+
+            val tv_up = TextView(this)
+            val linear_tv_up = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+            linear_tv_up.setMargins(150, 0, 0, 20)
+            tv_up.setText(listPrice[i])
+            tv_up.gravity=Gravity.CENTER
+            tv_up.typeface = resources.getFont(R.font.heycomic)
+            tv_up.typeface = Typeface.DEFAULT_BOLD       //Bold Text
+            tv_up.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 17F)
+            tv_up.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
+            tv_up.setLayoutParams(linear_tv_up)
+
+            val iv_product = ImageView(this)
+            val linear_iv_product = LinearLayout.LayoutParams(300, 300)
+            linear_iv_product.gravity = Gravity.CENTER
+            iv_product.setImageResource(R.color.grey)
+            iv_product.setLayoutParams(linear_iv_product)
+
+            val tv_down = TextView(this)
+            val linear_tv_down = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+            linear_tv_down.gravity = Gravity.CENTER
+            linear_tv_down.setMargins(0, 20, 0, 0)
+            tv_down.gravity=Gravity.CENTER
+            tv_down.typeface = resources.getFont(R.font.heycomic)
+            tv_down.typeface = Typeface.DEFAULT_BOLD       //Bold Text
+            tv_down.setText(listName[i])
+            tv_down.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 23F)
+            tv_down.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
+            tv_down.setLayoutParams(linear_tv_down)
+
+            product1.removeAllViews()
+            product1.addView(tv_up)
+            product1.addView(iv_product)
+            product1.addView(tv_down)
+
+
+            layout.addView(parent)
         }
 
-        val parent = LinearLayout(this)
-        val linear_parent = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
-        parent.gravity = Gravity.CENTER
-        linear_parent.gravity = Gravity.CENTER
-        parent.setLayoutParams(linear_parent)
-        parent.orientation = LinearLayout.HORIZONTAL
-        parent.setLayoutParams(linear_parent)
 
-        val product_pack = LinearLayout(this)
-        val linear_product_pack = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-        product_pack.gravity = Gravity.CENTER
-        linear_product_pack.gravity = Gravity.CENTER
-        product_pack.orientation = LinearLayout.HORIZONTAL
-        product_pack.setLayoutParams(linear_product_pack)
-
-        parent.addView(product_pack)
-
-
-        val product1 = LinearLayout(this)
-        val linear_product1 = LinearLayout.LayoutParams(500, 500)
-        linear_product1.setMargins(50, 50, 50, 50)
-        product1.gravity = Gravity.CENTER
-        product1.setBackgroundColor(getColor(R.color.colorSecondary))
-        product1.orientation = LinearLayout.VERTICAL
-        product1.setLayoutParams(linear_product1)
-
-        product_pack.addView(product1)
-
-        val tv_up = TextView(this)
-        val linear_tv_up = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-        linear_tv_up.setMargins(150, 0, 0, 20)
-        tv_up.setText(priceProduk)
-        tv_up.gravity=Gravity.CENTER
-        tv_up.typeface = resources.getFont(R.font.heycomic)
-        tv_up.typeface = Typeface.DEFAULT_BOLD       //Bold Text
-        tv_up.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 17F)
-        tv_up.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
-        tv_up.setLayoutParams(linear_tv_up)
-
-        val iv_product = ImageView(this)
-        val linear_iv_product = LinearLayout.LayoutParams(300, 300)
-        linear_iv_product.gravity = Gravity.CENTER
-        iv_product.setImageResource(R.color.grey)
-        iv_product.setLayoutParams(linear_iv_product)
-
-        val tv_down = TextView(this)
-        val linear_tv_down = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-        linear_tv_down.gravity = Gravity.CENTER
-        linear_tv_down.setMargins(0, 20, 0, 0)
-        tv_down.gravity=Gravity.CENTER
-        tv_down.typeface = resources.getFont(R.font.heycomic)
-        tv_down.typeface = Typeface.DEFAULT_BOLD       //Bold Text
-        tv_down.setText(namaProduk)
-        tv_down.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 23F)
-        tv_down.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
-        tv_down.setLayoutParams(linear_tv_down)
-
-        product1.removeAllViews()
-        product1.addView(tv_up)
-        product1.addView(iv_product)
-        product1.addView(tv_down)
-
-
-        layout.addView(parent)
     }
-
-//    fun buy(view: View?) {
-//        val bundle = Bundle()
-//        bundle.putString("nameProduct", namaProduk)
-//        bundle.putString("priceProduct", priceProduk)
-//
-//        val intent = Intent(this, BuyActivity::class.java)
-//        intent.putExtras(bundle)
-//        startActivity(intent)
-//    }
-
 
 }
