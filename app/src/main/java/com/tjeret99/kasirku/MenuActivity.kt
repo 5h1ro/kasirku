e69d9bbe248fe279c4e55c9b77cb327060681389
5h1ro <rajamodus7073@gmail.com>
2021-02-07 23:14:17 +0700

    UI Login, Menu

--- app/src/main/java/com/tjeret99/kasirku/MenuActivity.kt
+++ app/src/main/java/com/tjeret99/kasirku/MenuActivity.kt
@@ -62,6 +62,7 @@ class MenuActivity : AppCompatActivity() {
                     listPrice.add(priceAPI)
 
                 }
+                Toast.makeText(this, nameAPI, Toast.LENGTH_LONG).show()
                 produk("test")
             } catch (e: JSONException) {
                 e.printStackTrace()
@@ -113,10 +114,10 @@ class MenuActivity : AppCompatActivity() {
 
 
             val product1 = LinearLayout(this)
-            val linear_product1 = LinearLayout.LayoutParams(500, 500)
+            val linear_product1 = LinearLayout.LayoutParams(350, 350)
             linear_product1.setMargins(50, 50, 50, 50)
             product1.gravity = Gravity.CENTER
-            product1.setBackgroundColor(getColor(R.color.colorSecondary))
+//            product1.setBackgroundColor(getColor(R.color.colorSecondary))
             product1.orientation = LinearLayout.VERTICAL
             product1.setLayoutParams(linear_product1)
 
@@ -124,30 +125,30 @@ class MenuActivity : AppCompatActivity() {
 
             val tv_up = TextView(this)
             val linear_tv_up = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-            linear_tv_up.setMargins(150, 0, 0, 20)
+            linear_tv_up.setMargins(100, 0, 0, -50)
             tv_up.setText(listPrice[i])
             tv_up.gravity=Gravity.CENTER
             tv_up.typeface = resources.getFont(R.font.heycomic)
             tv_up.typeface = Typeface.DEFAULT_BOLD       //Bold Text
-            tv_up.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 17F)
+            tv_up.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 15F)
             tv_up.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
             tv_up.setLayoutParams(linear_tv_up)
 
             val iv_product = ImageView(this)
             val linear_iv_product = LinearLayout.LayoutParams(300, 300)
             linear_iv_product.gravity = Gravity.CENTER
-            iv_product.setImageResource(R.color.grey)
+            iv_product.setImageResource(R.drawable.bluder)
             iv_product.setLayoutParams(linear_iv_product)
 
             val tv_down = TextView(this)
             val linear_tv_down = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
             linear_tv_down.gravity = Gravity.CENTER
-            linear_tv_down.setMargins(0, 20, 0, 0)
+            linear_tv_down.setMargins(0, -30, 0, 0)
             tv_down.gravity=Gravity.CENTER
             tv_down.typeface = resources.getFont(R.font.heycomic)
             tv_down.typeface = Typeface.DEFAULT_BOLD       //Bold Text
             tv_down.setText(listName[i])
-            tv_down.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 23F)
+            tv_down.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20F)
             tv_down.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
             tv_down.setLayoutParams(linear_tv_down)
 
